9c176c2ccfc98b906ece4ef52242344a1c812523
gideonn12 <gideoneeman@icloud.com>
2024-03-08 15:02:41 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,68 @@
+/*Gideon Neeman 329924567*/
+#include <stdio.h>
+#include <stdlib.h>
+typedef unsigned char uchar;
+
+typedef struct cache_line_s {
+    uchar valid;
+    uchar frequency;
+    long int tag;
+    uchar *block;
+} cache_line_t;
+
+typedef struct cache_s {
+    uchar s;
+    uchar t;
+    uchar b;
+    uchar E;
+    cache_line_t **cache;
+} cache_t;
+cache_t initialize_cache(uchar s, uchar t, uchar b, uchar E);
+uchar read_byte(cache_t cache, uchar *start, long int off);
+void write_byte(cache_t cache, uchar *start, long int off, uchar new);
+void print_cache(cache_t cache) {
+    int S = 1 << cache.s;
+    int B = 1 << cache.b;
+
+    for (int i = 0; i < S; i++) {
+        printf("Set %d\n", i);
+        for (int j = 0; j < cache.E; j++) {
+            printf("%1d %d 0x%0*lx ", cache.cache[i][j].valid,
+                   cache.cache[i][j].frequency, cache.t, cache.cache[i][j].tag);
+            for (int k = 0; k < B; k++) {
+                printf("%02x ", cache.cache[i][j].block[k]);
+            }
+            puts("");
+        }
+    }
+}
+
+cache_t initialize_cache(uchar s, uchar t, uchar b, uchar E) {
+}
+
+uchar read_byte(cache_t cache, uchar *start, long int off) {
+}
+
+int main() {
+    int n;
+    printf("Size of data: ");
+    scanf("%d", &n);
+    uchar *mem = malloc(n);
+    printf("Input data >> ");
+    for (int i = 0; i < n; i++)
+        scanf("%hhd", mem + i);
+
+    int s, t, b, E;
+    printf("s t b E: ");
+    scanf("%d %d %d %d", &s, &t, &b, &E);
+    cache_t cache = initialize_cache(s, t, b, E);
+
+    while (1) {
+        scanf("%d", &n);
+        if (n < 0) break;
+        read_byte(cache, mem, n);
+    }
+    puts("");
+    print_cache(cache);
+    free(mem);
+}
